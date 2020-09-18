8eeebb84cb9dbe7dfa06f27311ed0e0bfc0b51c3
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-09-17 20:14:48 -0500

    Create Basic Pathfinding on enemies
    Create Basic Pathfinding on enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    private IEnumerator FollowPath()
+    {
+        print("Starting patrol");
+        foreach (Waypoint waipoint in path)
+        {
+            transform.position = waipoint.transform.position;
+            print("Patrol in position: " + waipoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+        print("End patrol");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WaypointEditor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class WaypointEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    void Update()
+    {
+        MoveInGrid();
+    }
+
+    void MoveInGrid()
+    {
+        Vector3 snapPos = CalculateGridPosition();
+        this.transform.position = snapPos;
+    }
+
+    private Vector3 CalculateGridPosition()
+    {
+        float posX = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        float posZ = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        PrintPosition(posX, posZ);
+
+        return new Vector3(posX, 0f, posZ);
+    }
+
+    void PrintPosition(float posX, float posZ)
+    { 
+        string labelText = posX / gridSize + "," + posZ / gridSize;
+        textMesh.text = labelText;
+
+        RenameWaypoint(labelText);
+    }
+
+    void RenameWaypoint(string name)
+    {
+        this.gameObject.name = name;
+    }
+}
