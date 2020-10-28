c5044fad2037943779e687bfd22f31c9f8d18584
Carlos Macias <lmaciasgonzalez@gmail.com>
2020-10-28 16:52:54 -0600

    Show particles when enemies are hitted or destroyed
    Show particles when enemies are hitted or destroyed

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticlesPrefab;
+    [SerializeField] ParticleSystem deathParticlesPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +23,15 @@ public class EnemyDamage : MonoBehaviour
     private void ProcessHit()
     {
         hitPoints--;
+        hitParticlesPrefab.Play();
     }
 
     private void ProcessKill()
     {
         if (hitPoints <= 0)
         {
+            var deathVfx = Instantiate(deathParticlesPrefab, transform.position, Quaternion.identity);
+            deathVfx.Play();
             Destroy(gameObject);
         }
     }
